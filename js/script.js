2994e4accf3c6a0a0a5e45176430951e7b96434d
Edson Bastos <edsonbastos2@hotmail.com>
2020-09-09 22:26:35 -0300

    Efeito de scroll suave nos menus internos

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function scrollSuave() {
+  const linksInternos = document.querySelectorAll(
+    '[data-scroll="suave"] a[href^="#"]'
+  );
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((item) => {
+    item.addEventListener("click", scrollToSection);
+  });
+}
+
+scrollSuave();
